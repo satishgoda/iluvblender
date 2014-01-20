55cf47b6c338cc1bbaaa18af0008351f4aebb924
Satish Goda <satishgoda@gmail.com>
2014-01-20 23:14:42 +0530

    Update area_spaces_navigation.py
    Implemented the interface required for AddOns

--- addons/ui/area_spaces_navigation.py
+++ addons/ui/area_spaces_navigation.py
@@ -1,3 +1,18 @@
+# <pep8 compliant>
+
+bl_info = {
+    "name": "Display Area Spaces in Header",
+    "author": "Satish Goda (iluvblender on BA, satishgoda@gmail.com)",
+    "version": (0, 1),
+    "blender": (2, 7, 0),
+    "location": "Most Editors -> Header",
+    "description": "Displays spaces that were activated in all the areas so that you can \
+                    conviniently switch between then",
+    "warning": "",
+    "category": "System"}
+
+"""Display Area Spaces in Header"""
+
 import bpy
 
 def draw(self, context):
@@ -6,12 +21,24 @@ def draw(self, context):
          op_props = row.operator('wm.context_set_enum', text=space.bl_rna.name.replace('Space', '').strip())
          op_props.data_path = "area.type"
          op_props.value = space.type
-         
-bpy.types.VIEW3D_HT_header.append(draw)
-bpy.types.CONSOLE_HT_header.append(draw)
-bpy.types.NODE_HT_header.append(draw)
-bpy.types.IMAGE_HT_header.append(draw)
-bpy.types.OUTLINER_HT_header.append(draw)
-bpy.types.PROPERTIES_HT_header.append(draw)
-bpy.types.TEXT_HT_header.append(draw)
 
+def register():
+    bpy.types.VIEW3D_HT_header.append(draw)
+    bpy.types.CONSOLE_HT_header.append(draw)
+    bpy.types.NODE_HT_header.append(draw)
+    bpy.types.IMAGE_HT_header.append(draw)
+    bpy.types.OUTLINER_HT_header.append(draw)
+    bpy.types.PROPERTIES_HT_header.append(draw)
+    bpy.types.TEXT_HT_header.append(draw)
+
+def unregister():
+    bpy.types.VIEW3D_HT_header.remove(draw)
+    bpy.types.CONSOLE_HT_header.remove(draw)
+    bpy.types.NODE_HT_header.remove(draw)
+    bpy.types.IMAGE_HT_header.remove(draw)
+    bpy.types.OUTLINER_HT_header.remove(draw)
+    bpy.types.PROPERTIES_HT_header.remove(draw)
+    bpy.types.TEXT_HT_header.remove(draw)
+    
+if __name__ == '__main__':
+    register()
