dc2ce97c807d97213cc53456e84903fd9255e409
Satish Goda <satishgoda@gmail.com>
2014-02-18 23:01:14 +0530

    Create testd.bat

---
+++ startup/testd.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+
+set BLENDER_USER_CONFIG=%CD%\config
+set BLENDER_USER_SCRIPTS=%CD%\scripts
+
+if not -%1-==-- (
+    CALL %BLENDER_INSTALL_DIR%\blender270a.lnk --debug --python %CD%\%1
+) ELSE (
+    CALL %BLENDER_INSTALL_DIR%\blender270a.lnk --debug
+)
