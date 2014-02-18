c55f3604c175a4af6a82b79405cbf4fb4cc6e4dc
Satish Goda <satishgoda@gmail.com>
2014-02-18 15:41:27 +0530

    Update myprefs.py

--- startup/myprefs.py
+++ startup/myprefs.py
@@ -26,6 +26,11 @@ system.use_scripts_auto_execute = True
 system.author = "First Last(emailid@domain) - learningblender3dsoftware.blogspot.in"
 
 
+for area in bpy.context.window_manager.windows[0].screen.areas:
+    area.show_menus = False
+    area.tag_redraw()
+
+
 # Save 'em
 bpy.ops.wm.save_homefile()
 bpy.ops.wm.save_userpref()
