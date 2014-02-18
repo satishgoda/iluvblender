fe54604c34595896b00885e83670928ad9749346
Satish Goda <satishgoda@gmail.com>
2014-02-18 23:00:27 +0530

    Update test.bat

--- startup/test.bat
+++ startup/test.bat
@@ -3,4 +3,8 @@
 set BLENDER_USER_CONFIG=%CD%\config
 set BLENDER_USER_SCRIPTS=%CD%\scripts
 
-CALL %BLENDER_INSTALL_DIR%\blender270a.lnk --debug
+if not -%1-==-- (
+    CALL %BLENDER_INSTALL_DIR%\blender270a.lnk --python %CD%\%1
+) ELSE (
+    CALL %BLENDER_INSTALL_DIR%\blender270a.lnk
+)
