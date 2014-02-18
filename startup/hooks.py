772134935aded68cce691c463b3ebc21608a2881
Satish Goda <satishgoda@gmail.com>
2014-02-18 22:59:33 +0530

    Update hooks.py

--- startup/hooks.py
+++ startup/hooks.py
@@ -1,10 +1,14 @@
 import bpy
-from bpy.app.handlers import persistent 
+from bpy.app.handlers import persistent
 
 @persistent
 def load_post_1(scene):
 	print("{0} - {1}".format("load_post", bpy.context.scene.name))
-	if bpy.data.filepath:
+	if not bpy.data.filepath:
+		print(" ** Untitled session ** ")
+	else:
 		print(bpy.data.filepath)
 	
 bpy.app.handlers.load_post.append(load_post_1)
+
+bpy.ops.wm.read_homefile()
