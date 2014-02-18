4dc62db35120a2c2cd0c9e4e6bd79b56bcb024be
Satish Goda <satishgoda@gmail.com>
2014-02-18 15:33:58 +0530

    Update myprefs.py

--- startup/myprefs.py
+++ startup/myprefs.py
@@ -1,10 +1,31 @@
+# -*- coding: utf-8 -*-
 import bpy
 
 userprefs = bpy.context.user_preferences
 
+
+# View (Interface)
 view = userprefs.view
 
 view.show_splash = False
 
+
+# File Paths
+filepaths = userprefs.filepaths
+
+filepaths.use_file_compression = True
+filepaths.show_thumbnails = True
+
+
+# System
+system = userprefs.system
+
+system.use_vertex_buffer_objects = True
+system.use_region_overlap = True
+system.use_scripts_auto_execute = True
+system.author = "First Last(emailid@domain) - learningblender3dsoftware.blogspot.in"
+
+
+# Save 'em
 bpy.ops.wm.save_homefile()
 bpy.ops.wm.save_userpref()
