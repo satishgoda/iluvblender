3d8a8a445f50100db7f1b2e57a5bdf00916fe215
Satish Goda <satishgoda@gmail.com>
2014-02-19 16:12:35 +0530

    Update myprefs.py

--- startup/myprefs.py
+++ startup/myprefs.py
@@ -3,16 +3,15 @@ import bpy
 
 
 # Scene Settings
+
 scene = bpy.context.scene
 
-scene.unit_settings.system = 'METRIC'
+if scene:
+    scene.unit_settings.system = 'METRIC'
+else:
+    print(type(bpy.context))
 
-
-# Rendering Settings
-render = scene.render
-
-render.engine = 'CYCLES'
-
+scene.render.engine = 'CYCLES'
 
 # User Preferences
 userprefs = bpy.context.user_preferences
@@ -37,14 +36,20 @@ system = userprefs.system
 system.use_vertex_buffer_objects = True
 system.use_region_overlap = True
 system.use_scripts_auto_execute = True
-system.author = "First Last(emailid@domain) - learningblender3dsoftware.blogspot.in"
+system.author = "Satish Goda (satishgoda@gmail) - learningblender3dsoftware.blogspot.in"
 
 
-# Hide the menus
 for area in bpy.context.window_manager.windows[0].screen.areas:
     area.show_menus = False
     area.tag_redraw()
 
+
+# Addons to auto-register
+myaddonpath = bpy.utils.user_resource('SCRIPTS', path='addons')
+if myaddonpath:
+    myaddonmodules = bpy.utils.modules_from_path(myaddonpath, set())
+    for module in myaddonmodules:
+        bpy.ops.wm.addon_enable(module=module.__name__)
 
 # Save 'em
 bpy.ops.wm.save_homefile()
