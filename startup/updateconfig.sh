20839eb2f11453dca7baffbf6cbea862c7ec2682
Satish Goda <satishgoda@gmail.com>
2014-02-20 13:32:42 +0530

    Create updateconfig.sh

---
+++ startup/updateconfig.sh
@@ -0,0 +1,10 @@
+#!/bin/tcsh
+
+setenv BLENDER_USER_CONFIG ./config
+setenv BLENDER_USER_SCRIPTS ./scripts
+
+if (-d $BLENDER_USER_CONFIG && -d $BLENDER_USER_SCRIPTS) then
+    blender270a --background --python handleAddons.py
+else
+    echo "CONFIG/SCRIPTS are missing"
+endif
