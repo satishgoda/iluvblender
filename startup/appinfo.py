cdbe4bc1e98e78b966b19e9d03d86ff5b3ab4c03
Satish Goda <satishgoda@gmail.com>
2014-02-21 15:15:41 +0530

    Update appinfo.py

--- startup/appinfo.py
+++ startup/appinfo.py
@@ -5,7 +5,20 @@ import inspect
 
 mros = inspect.getmro(type(bpy.app))
 
-app_attrs = sorted(set(dir(mros[0])) - set(dir(mros[1])))
+app_attrs = tuple(set(dir(mros[0])) - set(dir(mros[1])))
 
-for attr in app_attrs:
-    print(attr, getattr(bpy.app, attr))
+base_attrs = tuple(filter(lambda attr: attr.find('_') == -1, app_attrs))
+
+prefix_attrs = tuple(set(app_attrs) - set(base_attrs))
+
+base_prefix_attrs = tuple(map(lambda attr: tuple(filter(lambda pattr: pattr.startswith(attr), prefix_attrs)), base_attrs))
+
+print(sorted(base_attrs))
+print()
+print(sorted(prefix_attrs))
+print()
+print(base_prefix_attrs)
+
+
+#for attr in app_attrs:
+#    print(attr, getattr(bpy.app, attr))
