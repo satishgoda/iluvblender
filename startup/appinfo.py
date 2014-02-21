8a5c493ad5405cc7058c6e9fb64fcea6b703e85b
Satish Goda <satishgoda@gmail.com>
2014-02-21 12:42:04 +0530

    Update appinfo.py

--- startup/appinfo.py
+++ startup/appinfo.py
@@ -19,6 +19,6 @@ non_specials = list(filter(lambda s: not s.startswith('__'), names_in_scope))
 attr_value_pairs = dict(map(lambda attr: (attr, getattr(bpy.app, attr)), non_specials))
 
 for key, value in sorted(attr_value_pairs.items(), key=lambda t: t[0]):
-    print("{0}\n\t{1}".format(key, factory(value))
+    print("{0}\n\t{1}".format(key, factory(value)))
 
 
