183aab1e0f85be09d35fe8f9c7fabfdbf39b2fdf
Satish Goda <satishgoda@gmail.com>
2014-02-21 16:54:51 +0530

    Update appinfo.py

--- startup/appinfo.py
+++ startup/appinfo.py
@@ -3,21 +3,28 @@
 import bpy
 import inspect
 
+
 mros = inspect.getmro(type(bpy.app))
+
 
 attrs = tuple(set(dir(mros[0])) - set(dir(mros[1])))
 
-attrs_no_underscore = tuple(filter(lambda attr: attr.find('_') == -1, attrs))
 
-attrs_underscore = tuple(set(attrs) - set(attrs_no_underscore))
-
-base_prefix_attrs = tuple(filter(lambda base: set(filter(lambda pattr: pattr.startswith(base), attrs_underscore)), attrs_no_underscore))
-
-print(sorted(attrs_no_underscore))
-print()
-
-print(sorted(attrs_underscore))
-print()
-
-print(sorted(base_prefix_attrs))
-print()
+for attr in sorted(attrs):
+    print()
+    value = getattr(bpy.app, attr)
+    
+    if type(value) in (int, float, str, bool, tuple, bytes):
+        print("{0} - {1}".format(attr, value))
+    elif type(value) in (dict,):
+        print(attr)
+        for key, value in value.items():
+            print("\t{0} - {1}".format(key, type(value)))
+    elif isinstance(value, tuple):
+        print("{0} - ".format(attr))
+        mros = inspect.getmro(type(value))
+        tattrs = tuple(set(dir(mros[0])) - set(dir(mros[1])))
+        for tattr in tattrs:
+            print("\t{0} - {1}".format(tattr, getattr(value, tattr))) 
+    else:
+        print("{0} - {1}".format(attr, 'WOT EEZ DIS'))
