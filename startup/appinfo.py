2d3cbc79027a85ae758b50c70d15e0a9084b631a
Satish Goda <satishgoda@gmail.com>
2014-02-21 12:41:06 +0530

    Update appinfo.py

--- startup/appinfo.py
+++ startup/appinfo.py
@@ -2,6 +2,14 @@
 
 import bpy
 
+def factory(value):
+    if isinstance(value, bool):
+        return False
+    elif isinstance(value, str):
+        return '""'
+    else:
+        return value
+
 names_in_scope = dir(bpy.app)
 
 non_specials = list(filter(lambda s: not s.startswith('__'), names_in_scope))
@@ -9,5 +17,6 @@ non_specials = list(filter(lambda s: not s.startswith('__'), names_in_scope))
 attr_value_pairs = dict(map(lambda attr: (attr, getattr(bpy.app, attr)), non_specials))
 
 for key, value in sorted(attr_value_pairs.items(), key=lambda t: t[0]):
-    print("{0}\n\t{1}".format(key, value if value else "NOT AVAILABLE"))
+    print("{0}\n\t{1}".format(key, value if value else factory(value)))
+
 
