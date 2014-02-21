017c8d60080ebe79c8771194d933d9eabb05a8ce
Satish Goda <satishgoda@gmail.com>
2014-02-22 00:20:44 +0530

    Update appdatainfo.py
    Fixed the bug for cases like "driver_namespace" and "binary_path"

--- startup/appdatainfo.py
+++ startup/appdatainfo.py
@@ -17,12 +17,10 @@ attr_map = {}
 
 
 for attr in attrs_partitioned:
-    first = attr[0]
-    others = attr[1:]
-    
-    if not all(others) or (first not in attr_map):
+    first, others = attr[0], attr[1:]
+    if first not in attr_map:
         attr_map[first] = []
-    else:
+    if all(others):
         attr_map[first].extend(others[1:])
 
 
@@ -39,15 +37,22 @@ print(''.join(('\n', 'S'*79, '\n')))
 
 
 for key in keys_sorted:
-    print(key)
     items = attr_map[key]
     if items:
-        pair = lambda item: (item, getattr(bpy.app, key+'_'+item))
-        item_string_repr = lambda item: '   {0}\n      {1}'.format(*pair(item))
-        item_strings = map(item_string_repr, items)
-        print('\n'.join(item_strings))
+        if len(items) > 1:
+            print(key)
+            pair = lambda item: (item, getattr(bpy.app, key+'_'+item))
+            item_string_repr = lambda item: '   {0}\n      {1}'.format(*pair(item))
+            item_strings = map(item_string_repr, items)
+            print('\n'.join(item_strings))
+        else:
+            attr = key + '_' + items[0]
+            print('{0}\n   {1}'.format(attr, getattr(bpy.app, attr)))
     else:
-        print('   {0}'.format(getattr(bpy.app, key)))
+        attr = key
+        print('{0}\n   {1}'.format(attr, getattr(bpy.app, attr)))
+    
+    print()
 
 
 print(''.join(('\n', 'E'*79, '\n')))
