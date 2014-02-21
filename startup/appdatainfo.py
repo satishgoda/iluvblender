cc26d130850e3e9f9f51250f39db82bd826055f2
Satish Goda <satishgoda@gmail.com>
2014-02-21 23:12:51 +0530

    Update appdatainfo.py

--- startup/appdatainfo.py
+++ startup/appdatainfo.py
@@ -1,8 +1,10 @@
 import bpy
+
+import inspect
 import pprint
 
 
-mros = bpy.app.__class__.mro()
+mros = inspect.getmro(type(bpy.app))
 
 
 attrs = tuple(set(dir(mros[0])) - set(dir(mros[1])))
@@ -17,10 +19,25 @@ attr_map = {}
 for attr in attrs_partiotioned:
     first = attr[0]
     others = attr[1:]
-    if not all(others) or first not in attr_map:
+    if not all(others) or (first not in attr_map):
         attr_map[first] = []
     else:
         attr_map[first].extend(others[1:])
 
 
-pprint.pprint(attr_map)
+kwargs = {
+    'key': lambda key: len(attr_map[key]), 
+    'reverse': True
+}
+
+
+keys_sorted = sorted(attr_map.keys(), **kwargs)
+
+
+for key in keys_sorted:
+    print(key)
+    items = attr_map[key]
+    if items:
+        for item in items:
+            print('   {0}_{1}'.format(key, item))
+
