d4760df574398390b54284301c948409dfde8ea0
Satish Goda <satishgoda@gmail.com>
2014-02-21 12:40:42 +0530

    Update appinfo.py

--- startup/appinfo.py
+++ startup/appinfo.py
@@ -2,6 +2,11 @@
 
 import bpy
 
-for attr in filter(lambda s: s != 'driver_namespace', filter(lambda s: not s.startswith('__'),  dir(bpy.app))):
-    print("{0}\n\t{1}\n".format(attr, getattr(bpy.app, attr)))
+names_in_scope = dir(bpy.app)
 
+non_specials = list(filter(lambda s: not s.startswith('__'), names_in_scope))
+
+attr_value_pairs = dict(map(lambda attr: (attr, getattr(bpy.app, attr)), non_specials))
+
+for key, value in sorted(attr_value_pairs.items()):
+    print("{0}\n\t{1}".format(key, value))
