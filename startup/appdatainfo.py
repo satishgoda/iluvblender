7dae7b206469160907b50aa877743a07c83ba4fb
Satish Goda <satishgoda@gmail.com>
2014-02-23 07:45:09 +0530

    Update appdatainfo.py
    Use http://docs.python.org/3/library/itertools.html#itertools.groupby instead of homebrew logic for grouping attributes

--- startup/appdatainfo.py
+++ startup/appdatainfo.py
@@ -1,28 +1,22 @@
 import bpy
 
 import inspect
+import itertools
 import pprint
 
 
 mros = inspect.getmro(type(bpy.app))
 
 
-attrs = set(dir(mros[0])) - set(dir(mros[1]))
+attrs = sorted(set(dir(mros[0])) - set(dir(mros[1])))
 
 
-attrs_partitioned = tuple(attr.partition('_') for attr in sorted(attrs))
+prefix = lambda attr: attr.partition('_')[0]
 
 
-prefixes_unique = { partition[0] for partition in attrs_partitioned }
-
-
-attr_map = { prefix: [] for prefix in prefixes_unique }
-
-
-for attr in attrs_partitioned:
-    first, others = attr[0], attr[1:]
-    if all(others):
-        attr_map[first].append(others[-1])
+attr_map = {}
+for key, group in itertools.groupby(attrs, prefix):
+    attr_map[key] = list(group)
 
 
 kwargs = {
@@ -41,13 +35,13 @@ for key in keys_sorted:
     items = attr_map[key]
     if len(items) > 1:
         print(key)
-        pair = lambda item: (item, getattr(bpy.app, key+'_'+item))
+        pair = lambda item: (item, getattr(bpy.app, item))
         item_string_repr = lambda item: '   {0}\n      {1}'.format(*pair(item))
         item_strings = map(item_string_repr, items)
         print('\n'.join(item_strings))
     else:
         if len(items) == 1:
-            attr = key + '_' + items[0]
+            attr = items[0]
         else:
             attr = key
         print('{0}\n   {1}'.format(attr, getattr(bpy.app, attr)))
