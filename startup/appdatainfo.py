3fe14a2d4685e8f649853f4ad48edcd7084a83ed
Satish Goda <satishgoda@gmail.com>
2014-02-23 11:40:15 +0530

    Update appdatainfo.py
    Starting to move data together so that I can OOPsify shortly

--- startup/appdatainfo.py
+++ startup/appdatainfo.py
@@ -25,30 +25,34 @@ mros = inspect.getmro(type(bpy.app))
 attrs = set.difference(*map(setdir, mros[:2]))
 
 attr_map = {}
+attrs_grouped = set()
+attrs_plain = set()
 
 for key, group in itertools.groupby(sorted(attrs), prefix):
     grouped = list(group)
-    k, v = (key, grouped) if len(grouped) > 1 else (grouped[0], None)
+    k, v = None, None
+    if len(grouped) > 1:
+        k, v = (key, grouped)
+        attrs_grouped.add(key)     
+    else: 
+        k, v = (grouped[0], None)
+        attrs_plain.add(k)
+    
     attr_map[k] = v
 
 
-# Organize
-
-grouped = sorted(filter(attr_map.get, attr_map))
-plain = sorted(set(attr_map.keys()) - set(grouped))
+# Print
 
 buffer = []
 
-for prefix in grouped:
+for prefix in sorted(attrs_grouped):
     items = attr_map[prefix]
     buffer.append(prefix.upper()+'\n')
     buffer.extend(list(map(attr_repr_indent, items)))
 
-for attr in plain:
+for attr in sorted(attrs_plain):
    buffer.append(attr_repr_normal(attr))
 
-
-# Print
 
 print(''.join(('\n', 'S'*79, '\n')))
 
