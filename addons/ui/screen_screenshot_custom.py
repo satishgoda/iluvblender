d53fd7b4dd7e3bc357992016b05472cafeb31e23
Satish Goda <satishgoda@gmail.com>
2014-02-25 14:50:53 +0530

    Update screen_screenshot_custom.py

--- addons/ui/screen_screenshot_custom.py
+++ addons/ui/screen_screenshot_custom.py
@@ -8,23 +8,32 @@ class ScreenshotsCustom(bpy.types.Operator):
     bl_options = {'REGISTER'}
     
     def __init__(self):
-        print("Initializing")        
+        print("Initializing " + self.bl_idname)        
     
     def execute(self, context):
-        print("Executing")
+        print("Executing " + self.bl_idname)
+        
+        overrides = {}
+        overrides['window'] = bpy.context.window
+        
+        for area in context.screen.areas:
+            overrides['area'] = area
+            overrides['region'] = area.regions[1]
+            bpy.ops.screen.screenshot(overrides, filepath="//{0}.png".format(area.type), full=False)
+        
         return {'FINISHED'}
     
     def __del__(self):
-        print("Destructing")
+        print("Destructing " + self.bl_idname)
 
 
 def register():
     bpy.utils.register_class(ScreenshotsCustom)
-  
+
 
 def unregister():
     bpy.utils.unregister_class(ScreenshotsCustom)
-    
+
 
 if __name__ == '__main__':
     register()
