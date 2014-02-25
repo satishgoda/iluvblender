4efd40e5ce4f5962c738f779d4ff73d3700bb913
Satish Goda <satishgoda@gmail.com>
2014-02-25 15:08:06 +0530

    Update screen_screenshot_custom.py

--- addons/ui/screen_screenshot_custom.py
+++ addons/ui/screen_screenshot_custom.py
@@ -1,5 +1,25 @@
 import bpy
 
+import os
+
+_filepath = bpy.data.filepath
+
+_screenshot = dict()
+_screenshot['filename'] = bpy.path.display_name_from_filepath(_filepath) if _filepath else 'untitled'
+_screenshot['filename_ext'] = 'png'
+_screenshot['dirname'] = os.path.dirname(_filepath) if _filepath else os.getcwd()
+_screenshot['filename_suffix_char'] = '_'
+_screenshot['filename_suffix'] = None
+
+def _get_filepath(screenshot):
+    import os
+    s = screenshot
+    filename = s['filename']
+    if s['filename_suffix']:
+        filename += s['filename_suffix_char'] + s['filename_suffix']
+    filename += '.' + s['filename_ext']
+    filepath = os.path.join(s['dirname'], filename)
+    return filepath
 
 class ScreenshotsCustom(bpy.types.Operator):
     """Create and save screenshots of different areas"""
@@ -16,10 +36,18 @@ class ScreenshotsCustom(bpy.types.Operator):
         overrides = {}
         overrides['window'] = bpy.context.window
         
+        kwargs = { 'full': False }
+        
         for area in context.screen.areas:
             overrides['area'] = area
             overrides['region'] = area.regions[1]
-            bpy.ops.screen.screenshot(overrides, filepath="//{0}.png".format(area.type), full=False)
+            
+            screenshot = _screenshot.copy()
+            screenshot['filename_suffix'] = area.type
+            
+            kwargs['filepath'] = _get_filepath(screenshot)
+
+            bpy.ops.screen.screenshot(overrides, **kwargs)
         
         return {'FINISHED'}
     
@@ -30,11 +58,11 @@ class ScreenshotsCustom(bpy.types.Operator):
 def register():
     bpy.utils.register_class(ScreenshotsCustom)
 
-
+  
 def unregister():
     bpy.utils.unregister_class(ScreenshotsCustom)
 
-
+    
 if __name__ == '__main__':
     register()
     bpy.ops.screen.screenshot_custom()
