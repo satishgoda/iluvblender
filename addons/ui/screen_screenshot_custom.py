f3e156a939f219b9169f44fa428c42229c1946ef
Satish Goda <satishgoda@gmail.com>
2014-02-26 15:47:51 +0530

    Enable third capture mode...
    ... and also move code to functions so as to reuse

--- addons/ui/screen_screenshot_custom.py
+++ addons/ui/screen_screenshot_custom.py
@@ -10,7 +10,34 @@ bl_info = {
     "category": "Screen"
 }
 
+
 import bpy
+
+
+def _screen(self, context, screenshot):
+    bpy.ops.screen.screenshot(filepath=screenshot.filepath)
+
+
+def _screen_all_areas(self, context, screenshot):
+    overrides = {}
+
+    overrides['window'] = context.window
+    overrides['screen'] = context.screen
+    overrides['scene'] = context.scene
+    
+    kwargs = { 'full': False }
+
+    for area in context.screen.areas:
+        overrides['area'] = area
+        overrides['region'] = area.regions[1]
+        overrides['space_data'] = area.spaces.active
+
+        screenshot.filename_suffix = area.type
+
+        kwargs['filepath'] = screenshot.filepath
+
+        bpy.ops.screen.screenshot(overrides, **kwargs)
+
 
 class ScreenshotsCustom(bpy.types.Operator):
     """Create and save screenshots of different areas"""
@@ -38,33 +65,22 @@ class ScreenshotsCustom(bpy.types.Operator):
     def execute(self, context):
         print("Executing " + self.bl_idname)
         
-        overrides = {}
-        overrides['window'] = context.window
-        overrides['screen'] = context.screen
-        overrides['scene'] = context.scene
-        
         import screenshot
         screenshot = screenshot.Screenshot()
 
         if self.capture_mode == 'SCREEN':
-            bpy.ops.screen.screenshot(filepath=screenshot.filepath)
+            _screen(self, context, screenshot)
         elif self.capture_mode == 'SCREEN_ALL_AREAS':
-            kwargs = { 'full': False }
-
-            for area in context.screen.areas:
-                overrides['area'] = area
-                overrides['region'] = area.regions[1]
-                overrides['space_data'] = area.spaces.active
-
-                screenshot.filename_suffix = area.type
-
-                kwargs['filepath'] = screenshot.filepath
-
-                bpy.ops.screen.screenshot(overrides, **kwargs)
+            _screen_all_areas(self, context, screenshot)
+        elif self.capture_mode == 'SCREEN_AND_ALL_AREAS':
+            _screen_all_areas(self, context, screenshot)
+            _screen(self, context, screenshot)
         else:
-            self.report({'INFO'}, self.capture_mode)
+            self.report({'ERROR'}, "Save Screenshot Custom: No other capture modes supported")
+            return {'CANCELLED'}
 
         self.report({'INFO'}, "Screenshot saved in {0}".format(screenshot.dirname))
+        
         context.window_manager.clipboard = screenshot.dirname
         
         return {'FINISHED'}
@@ -91,22 +107,23 @@ def register():
 
     setattr(kmi.properties, 'capture_mode', 'SCREEN_ALL_AREAS')
 
+    kmi = keymap.keymap_items.new('screen.screenshot_custom', 'C', 'PRESS', ctrl=True, oskey=True)
+
+    setattr(kmi.properties, 'capture_mode', 'SCREEN_ALL_AREAS')
+
   
 def unregister():
     keyconfigs = bpy.context.window_manager.keyconfigs
 
     keymap = keyconfigs.addon.keymaps['Screen']
 
-    kmi = keymap.keymap_items[ScreenshotsCustom.bl_idname]
-    keymap.keymap_items.remove(kmi)
-    del kmi
+    for i in range(3):
+        kmi = keymap.keymap_items[ScreenshotsCustom.bl_idname]
+        keymap.keymap_items.remove(kmi)
 
-    kmi = keymap.keymap_items[ScreenshotsCustom.bl_idname]
-    keymap.keymap_items.remove(kmi)
-    del kmi
-    
     bpy.utils.unregister_class(ScreenshotsCustom)
-    
+
+
 if __name__ == '__main__':
     register()
     #bpy.ops.screen.screenshot_custom()
