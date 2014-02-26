d5a858b20e2f9d131d02ab7153beaf4d6265a5c5
Satish Goda <satishgoda@gmail.com>
2014-02-26 15:26:21 +0530

    Enable/Disable wm.keyconfigs.addon.keymaps
    KeyConfigurations.addon
    Key configuration that can be extended by addons, and is added to the active configuration when handling events
    
    KeyConfigurations.user
    Final key configuration that combines keymaps from the active and addon configurations, and can be edited by the user

--- addons/ui/screen_screenshot_custom.py
+++ addons/ui/screen_screenshot_custom.py
@@ -63,6 +63,9 @@ class ScreenshotsCustom(bpy.types.Operator):
                 bpy.ops.screen.screenshot(overrides, **kwargs)
         else:
             self.report({'INFO'}, self.capture_mode)
+
+        self.report({'INFO'}, "Screenshot saved in {0}".format(screenshot.dirname))
+        context.window_manager.clipboard = screenshot.dirname
         
         return {'FINISHED'}
     
@@ -72,11 +75,37 @@ class ScreenshotsCustom(bpy.types.Operator):
 
 def register():
     bpy.utils.register_class(ScreenshotsCustom)
+    
+    keyconfigs = bpy.context.window_manager.keyconfigs
+
+    if 'Screen' not in keyconfigs.addon.keymaps:
+        keyconfigs.addon.keymaps.new('Screen')
+
+    keymap = keyconfigs.addon.keymaps['Screen']
+    
+    kmi = keymap.keymap_items.new('screen.screenshot_custom', 'C', 'PRESS', oskey=True)
+    
+    setattr(kmi.properties, 'capture_mode', 'SCREEN')
+
+    kmi = keymap.keymap_items.new('screen.screenshot_custom', 'C', 'PRESS', shift=True, oskey=True)
+
+    setattr(kmi.properties, 'capture_mode', 'SCREEN_ALL_AREAS')
 
   
 def unregister():
-    bpy.utils.unregister_class(ScreenshotsCustom)
+    keyconfigs = bpy.context.window_manager.keyconfigs
 
+    keymap = keyconfigs.addon.keymaps['Screen']
+
+    kmi = keymap.keymap_items[ScreenshotsCustom.bl_idname]
+    keymap.keymap_items.remove(kmi)
+    del kmi
+
+    kmi = keymap.keymap_items[ScreenshotsCustom.bl_idname]
+    keymap.keymap_items.remove(kmi)
+    del kmi
+    
+    bpy.utils.unregister_class(ScreenshotsCustom)
     
 if __name__ == '__main__':
     register()
