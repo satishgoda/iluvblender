8141500d0c797cef7138a1e8e30cd4c3c849cdf9
Satish Goda <satishgoda@gmail.com>
2014-02-26 12:33:56 +0530

    Update createconfig.sh

--- startup/createconfig.sh
+++ startup/createconfig.sh
@@ -3,14 +3,23 @@
 setenv BLENDER_USER_CONFIG ./config
 setenv BLENDER_USER_SCRIPTS ./scripts
 
-if (-d config) then
-    rm -rfv config
+if (-d $BLENDER_USER_CONFIG) then
+    if (-f $BLENDER_USER_CONFIG/startup.blend) then
+        rm -fv $BLENDER_USER_CONFIG/startup.blend
+    endif
+    if (-f $BLENDER_USER_CONFIG/userpref.blend) then
+        rm -fv $BLENDER_USER_CONFIG/userpref.blend
+    endif
 endif
 
-if (-d scripts) then
-    rm -rfv scripts
-endif
+#if (-d scripts) then
+#    rm -rfv scripts
+#endif
 
-mkdir config scripts -v && blender270a --debug --background --python myprefs.py
+#mkdir config -v && blender270 --debug --background --python myprefs.py
+#mkdir config -v && blender270 --background --python myprefs.py
+
+blender270 --background --python myprefs.py
 
 tree --dirsfirst --noreport -C
+
