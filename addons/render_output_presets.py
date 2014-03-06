b6fafbf21b117400cbd630ab350a483756492e74
Satish Goda <satishgoda@gmail.com>
2014-03-06 12:01:33 +0530

    pep8 compliant code
    Used http://pych.atomidata.com/code to check
    
    More info at http://wiki.blender.org/index.php/Dev:2.5/Py/Scripts/Guidelines/Addons#See_also

--- addons/render_output_presets.py
+++ addons/render_output_presets.py
@@ -1,3 +1,23 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+# <pep8 compliant>
+
 import bpy
 
 from bl_operators import presets
@@ -33,21 +53,19 @@ class RENDER_MT_output(bpy.types.Menu):
 def odraw(self, context):
     layout = self.layout
 
-    scene = context.scene
-    rd = scene.render
-
     row = layout.row(align=True)
     row.menu("RENDER_MT_output", text=bpy.types.RENDER_MT_output.bl_label)
     row.operator("render.preset_add", text="", icon='ZOOMIN')
-    row.operator("render.preset_add", text="", icon='ZOOMOUT').remove_active = True
+    opprops = row.operator("render.preset_add", text="", icon='ZOOMOUT')
+    opprops.remove_active = True
 
-    
+
 def register():
     bpy.utils.register_class(AddPresetOutput)
     bpy.utils.register_class(RENDER_MT_output)
     bpy.types.RENDER_PT_output.prepend(odraw)
 
-    
+
 def unregister():
     bpy.types.RENDER_PT_output.remove(odraw)
     bpy.utils.unregister_class(AddPresetOutput)
