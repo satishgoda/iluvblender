6a23f014aea1d1b29d6d012621d376cc82254788
Satish Goda <satishgoda@gmail.com>
2014-03-10 13:26:23 +0530

    Update bappboot.py

--- sandbox/apps/bappboot.py
+++ sandbox/apps/bappboot.py
@@ -1,11 +1,5 @@
-import bpy
-
 import bapp
 
-bapp.Debug()("BOOTING UP")
+print("BOOTING UP")
 
-bpy.app.handlers.load_post.insert(0, bapp.load_modules)
-
-bpy.ops.wm.read_homefile()
-
-bpy.context.user_preferences.view.show_splash = False
+bapp.boot()
