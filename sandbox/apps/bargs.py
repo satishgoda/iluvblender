c8310d4c05430df67ea89634485b36a59e2c5415
Satish Goda <satishgoda@gmail.com>
2014-03-12 17:24:59 +0530

    Update bargs.py

--- sandbox/apps/bargs.py
+++ sandbox/apps/bargs.py
@@ -1,22 +1,48 @@
+class ProgramLauncher(object):
+    import os
+    cwd = os.getcwd()
+
+    def __init__(self, name, path):
+        self.name = name
+        self.path = path
+
+    def __str__(self):
+        l = self._print()
+        l += '\n    located at {0}'.format(self.path)
+        l += '\n    CWD: {0}'.format(self.cwd)
+        l += '\n'
+        return l
+
+
+class ProgramLauncherBinary(ProgramLauncher):
+    def _print(self):
+        return "Launched as binary {0}".format(self.name)
+
+
+class ProgramLauncherSymlink(ProgramLauncher):
+    def __init__(self, name, path):
+        import os
+        super(ProgramLauncherSymlink, self).__init__(name, os.readlink(path))
+
+    def _print(self):
+        return "Launched via a symlink {0}".format(self.name)
+
 
 class ProgramDetails:
     def __init__(self):
         import sys
-        from os import readlink, getcwd
         from os.path import isabs, abspath, exists, islink, basename
-        import shutil
 
-        self.launcher = arg0 = sys.argv[0]
+        arg0 = sys.argv[0]
 
         program_path =  arg0 if isabs(arg0) else abspath(arg0)
 
-        self.cwd = getcwd()
-
         if exists(program_path):
             if islink(program_path):
-                self.path = readlink(shutil.which(program_path))
+                self.launcher = ProgramLauncherSymlink(arg0, program_path)
             else:
-                self.path = program_path
+                self.launcher = ProgramLauncherBinary(arg0, program_path)
         else:
             basename = basename(program_path)
-            self.path = readlink(shutil.which(basename))
+            import shutil
+            self.launcher = ProgramLauncherSymlink(arg0, shutil.which(basename))
