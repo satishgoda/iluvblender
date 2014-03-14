5bf3bf27b2a454eda1b35cd8133942ebbed69e04
Satish Goda <satishgoda@gmail.com>
2014-03-14 11:59:12 +0530

    Update bpyTypesObject.py

--- sandbox/source/rna/bpyTypesObject.py
+++ sandbox/source/rna/bpyTypesObject.py
@@ -8,10 +8,18 @@ from pprint import pprint
 
 property_type = lambda prop: prop.type
 
-theType = bpy.types.Object
+theTypeRna = bpy.types.Object.bl_rna
 
-iterable = sorted(get_direct_properties(theType.bl_rna), key=property_type)
 
-propmap = { key: tuple(group) for key, group in groupby(iterable, property_type) }
+properties = sorted(theTypeRna.properties, key=property_type)
+
+properties_direct = sorted(get_direct_properties(theTypeRna), key=property_type)
+
+
+pprint(set.difference(*(map(set, (properties, properties_direct)))))
+
+
+propmap = { key: tuple(group) for key, group in groupby(properties_direct, property_type) }
+
 
 pprint(propmap)
