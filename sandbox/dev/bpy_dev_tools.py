05292fe94a3fdeb409b76e1269a323481fadd895
Satish Goda <satishgoda@gmail.com>
2014-03-14 13:51:44 +0530

    Update bpy_dev_tools.py

--- sandbox/dev/bpy_dev_tools.py
+++ sandbox/dev/bpy_dev_tools.py
@@ -10,6 +10,8 @@ import bpy
 
 import rna_info
 
+HEADER_TYPES = filter(lambda identifier: identifier.endswith('_HT_header'), dir(bpy.types))
+
 
 class ContextProperties(bpy.types.Operator):
     bl_idname = 'debug.context'
@@ -17,8 +19,14 @@ class ContextProperties(bpy.types.Operator):
     bl_description = 'View the properties of the current context'
     
     def execute(self, context):
-        print(rna_info.get_direct_properties(context.bl_rna))
+        print(rna_info.get_direct_properties(context.space_data.bl_rna))
         return {'FINISHED'}
+
+
+def ALL_HT_debug_context_draw(self, context):
+    layout = self.layout
+    row = layout.row()
+    row.operator('debug.context')
 
 
 _operators = (
@@ -30,11 +38,16 @@ def register():
     for operator in _operators:
         bpy.utils.register_class(operator)
 
+    for header_identifier in HEADER_TYPES:
+        eval('.'.join(('bpy.types', header_identifier))).append(ALL_HT_debug_context_draw)
+
 
 def unregister():
     for operator in _operators:
         bpy.utils.unregister_class(operator)
 
+    for header_identifier in HEADER_TYPES:
+        eval('.'.join(('bpy.types', header_identifier))).remove(ALL_HT_debug_context_draw)
 
 if __name__ == '__main__':
     register()
