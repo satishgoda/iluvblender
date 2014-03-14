322d985ef2b9d9d39e64be09be3ac2e90a605ce1
Satish Goda <satishgoda@gmail.com>
2014-03-14 12:42:22 +0530

    Create bpy_dev_tools.py

---
+++ sandbox/dev/bpy_dev_tools.py
@@ -0,0 +1,21 @@
+bl_info = {
+    'name': 'Development tools for Blender',
+    'description': 'Registers operators, panels, menus etc to abet development and introspection',
+    'category': 'Development',
+}
+
+"""Registers operators, panels, menus etc to abet development and introspection"""
+
+import bpy
+
+
+def register():
+    pass
+
+
+def unregister():
+    pass
+
+
+if __name__ == '__main__':
+    register()
