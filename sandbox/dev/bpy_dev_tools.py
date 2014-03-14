d9cccdb4ca95f1ee144709bb9d3bbe7fef31de39
Satish Goda <satishgoda@gmail.com>
2014-03-14 12:49:37 +0530

    Update bpy_dev_tools.py

--- sandbox/dev/bpy_dev_tools.py
+++ sandbox/dev/bpy_dev_tools.py
@@ -8,13 +8,32 @@ bl_info = {
 
 import bpy
 
+import rna_info
+
+
+class ContextProperties(bpy.types.Operator):
+    bl_idname = 'debug.context'
+    bl_label = 'Debug Context Properties'
+    bl_description = 'View the properties of the current context'
+    
+    def execute(self, context):
+        print(rna_info.get_direct_properties(context.bl_rna))
+        return {'FINISHED'}
+
+
+_operators = (
+    ContextProperties,
+)
+
 
 def register():
-    pass
+    for operator in _operators:
+        bpy.utils.register_class(operator)
 
 
 def unregister():
-    pass
+    for operator in _operators:
+        bpy.utils.unregister_class(operator)
 
 
 if __name__ == '__main__':
