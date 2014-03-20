8371524a2dfc3c8c9abf7201442f036ec6e0b3b7
Satish Goda <satishgoda@gmail.com>
2014-03-20 15:50:25 +0530

    Create debug_app_debug_flags.py

---
+++ addons/debug/debug_app_debug_flags.py
@@ -0,0 +1,17 @@
+bl_info = {
+    'name': 'Application Debugging Flags',
+    'description': 'User interface to the application debug flags (bpy.app.debug[_*])',
+    'category': 'Debugging',
+}
+
+
+def register():
+    pass
+
+
+def unregister():
+    pass
+
+
+if __name__ == '__main__':
+    register()
