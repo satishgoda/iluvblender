2d975e88a789a6d77e1075bd2699120e08d27332
Satish Goda <satishgoda@gmail.com>
2014-03-20 16:12:01 +0530

    Update debug_app_debug_flags.py

--- addons/debug/debug_app_debug_flags.py
+++ addons/debug/debug_app_debug_flags.py
@@ -5,8 +5,45 @@ bl_info = {
 }
 
 
+import bpy
+
+
+class BAppRuntime(object):
+    bclass = bpy.app
+    _filter = lambda attr: attr
+        
+    @classmethod
+    def getattrs(cls):
+        setdir = lambda bclass: set(dir(bclass))
+        attrs = set.difference(*map(setdir, type(cls.bclass).mro()[:2]))
+        return sorted(filter(cls._filter, attrs))
+
+    @classmethod
+    def getattrdoc(cls, attr):
+        import inspect
+        return inspect.getdoc(vars(cls.bclass.__class__)[attr]) 
+
+
+class BAppRuntimeDebug(BAppRuntime):
+    _filter = lambda attr: attr.startswith('debug')
+
+    @classmethod
+    def getenumeration(cls):
+        enum_items = []
+        for attr in cls.getattrs():
+            enum = attr.split('_')[1] if attr.count('_') else attr
+            enum = enum.upper()
+            name = enum.capitalize()
+            description = cls.getattrdoc(attr)
+            attr_value = getattr(bpy.app, attr)
+            icon = 'CHECKBOX_HLT' if attr_value else 'CHECKBOX_DEHLT'
+            enum_items.append((enum, name, description, icon))
+        return enum_items
+
+
 def register():
-    pass
+    import pprint
+    pprint.pprint(BAppRuntimeDebug.getenumeration())
 
 
 def unregister():
