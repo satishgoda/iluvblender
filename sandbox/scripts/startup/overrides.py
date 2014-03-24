99793edb7238aa0294d167673fd29e8c00a3ed4d
Satish Goda <satishgoda@gmail.com>
2014-03-24 13:09:25 +0530

    Update overrides.py

--- sandbox/scripts/startup/overrides.py
+++ sandbox/scripts/startup/overrides.py
@@ -15,6 +15,10 @@ def ALL_HT_header_draw_override(self, context):
         text = "{} ({})".format(area.type, layout.enum_item_description(area, 'type', area.type))
         icon = area.bl_rna.properties['type'].enum_items[area.type].icon
         row = layout.row()
+        row.alert = True
+        row.operator_context = 'EXEC_DEFAULT'
+        row.operator('wm.debug_menu', text='', icon='LOOP_BACK').debug_value = 0
+        row.operator_context = 'INVOKE_DEFAULT'
         row.label(text, icon=icon)
     else:
         header_map[context.area.type](self, context)
