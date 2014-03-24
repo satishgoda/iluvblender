463c4303f3731941d25e39ff0fc7162cd0f59067
Satish Goda <satishgoda@gmail.com>
2014-03-24 12:22:37 +0530

    Update overrides.py
    blender --debug-value 1

--- sandbox/scripts/startup/overrides.py
+++ sandbox/scripts/startup/overrides.py
@@ -7,9 +7,17 @@ bl_ui_headers = tuple(filter(bl_ui_headers_only, bpy.types.Header.__subclasses__
 header_map = {header.bl_space_type: header.draw for header in bl_ui_headers }
 
 
-def ALL_HT_header_draw_override(self, context):
+def ALL_HT_header_draw_override(self, context):  
+    area = context.area
+    area_type_enum_item = area.bl_rna.properties['type'].enum_items[area.type]
+    icon = area_type_enum_item.icon
+    text = "{} ({})".format(area.type, area_type_enum_item.description)
+    
+    layout = self.layout
+    row = layout.row()
+    
     if bpy.app.debug_value == 1:
-        self.layout.label(context.area.type)
+        row.label(text, icon=icon)
     else:
         header_map[context.area.type](self, context)
 
