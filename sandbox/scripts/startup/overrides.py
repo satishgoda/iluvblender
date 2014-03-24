2e2bb7855cff42811840676e70368ff77de07295
Satish Goda <satishgoda@gmail.com>
2014-03-24 11:57:50 +0530

    Update overrides.py

--- sandbox/scripts/startup/overrides.py
+++ sandbox/scripts/startup/overrides.py
@@ -2,31 +2,25 @@ import bpy
 
 
 bl_ui_headers_only = lambda header: header.__module__.startswith('bl_ui')
-bl_ui_headers = filter(bl_ui_headers_only, bpy.types.Header.__subclasses__())
-bl_ui_header_names = tuple(header.__name__ for header in bl_ui_headers)
+bl_ui_headers = tuple(filter(bl_ui_headers_only, bpy.types.Header.__subclasses__()))
 
-header_map = {}
-for header_name in bl_ui_header_names:
-    header = getattr(bpy.types, header_name)
-    header_map[header.bl_space_type] = header.draw
+header_map = {header.bl_space_type: header.draw for header in bl_ui_headers }
 
 
 def ALL_HT_header_draw_override(self, context):
     if bpy.app.debug:
-        print("Drawing custom header")
         self.layout.label(context.area.type)
     else:
-        print("Drawing original header")
         header_map[context.area.type](self, context)
 
 
 def register():
-    for header_name in bl_ui_header_names:
-        getattr(bpy.types, header_name).draw = ALL_HT_header_draw_override
+    for header in bl_ui_headers:
+        header.draw = ALL_HT_header_draw_override
 
 
 def unregister():
-    for header_name in bl_ui_header_names:
-        getattr(bpy.types, header_name).draw = header_map[getattr(bpy.types, header_name).bl_space_type]
+    for header in bl_ui_headers:
+        header.draw = header_map[header.bl_space_type]
         
 
