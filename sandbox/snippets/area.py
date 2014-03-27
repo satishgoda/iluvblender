4211517f4bb9654e8218b58f0db7fefb120f8eae
Satish Goda <satishgoda@gmail.com>
2014-03-27 16:55:15 +0530

    Update area.py

--- sandbox/snippets/area.py
+++ sandbox/snippets/area.py
@@ -1,22 +1,24 @@
 
 import bpy
 
-def explain(self):
+def explain(cls):
     """Explain in detail about this object"""
-    print(self.bl_rna.identifier, "belonging to", self.id_data.bl_rna.description)
-    print('\t', self.bl_rna.name)
-    print('\t', self.bl_rna.description)
+    print(cls.bl_rna.identifier)
+    print('\t', cls.bl_rna.name)
+    print('\t', cls.bl_rna.description)
     print()
-    for prop in self.bl_rna.properties:
+    for prop in cls.bl_rna.properties:
         if prop.identifier in ('rna_type',):
             continue
         print("{} ({})\n\t{}\n".format(prop.identifier, prop.name, prop.description))
 
-
-bpy.types.Area.basic_info = (lambda area, flag=False: "You get nothing" if flag else (area.type, len(area.regions)))
-bpy.types.Area.basic_info.__doc__ = "Get the basic information of an area"
-bpy.types.Area.basic_info.__annotations__['flag'] = 'Flag to flag an issue'
+def register():
+    # http://blender.stackexchange.com/questions/1153/is-it-possible-to-add-a-custom-property-on-bpy-types-sequence
+    bpy.types.Area.basic_info = (lambda area, flag=False: "You get nothing" if flag else (area.type, len(area.regions)))
+    bpy.types.Area.basic_info.__doc__ = "Get the basic information of an area"
+    bpy.types.Area.basic_info.__annotations__['flag'] = 'Flag to flag an issue'
     
-bpy.types.Area.explain = explain
-
+    bpy.types.Area.explain = classmethod(explain)
     
+def unregister():
+    pass
