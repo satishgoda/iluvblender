06f3d746d84361c1142f3f9ab6aac2288c26a953
Satish Goda <satishgoda@gmail.com>
2014-04-02 12:41:02 +0530

    Update btypes_meta.py
    >>> import imp
    >>> 
    >>> imp.reload(subclasses)
    <module 'subclasses' from '/home/satishg/blender/testing/test1/btypes_meta.blend/subclasses.py'>
    
    >>> subclasses.main()
    ABCMeta
        abc
        /home/satishg/bin/blenderinstalldir/blender-2.70-linux-glibc211-x86_64/2.70/python/lib/python3.3/abc.py
    
    bpy_struct_meta_idprop
        builtins
        <module 'builtins' (built-in)> is a built-in class
    
    RNAMeta
        bpy_types
        /home/satishg/bin/blenderinstalldir/blender-2.70-linux-glibc211-x86_64/2.70/scripts/modules/bpy_types.py
    
    MyMeta
        mytypes
        /home/satishg/musesatishg/blender/testing/pipeline/test1/btypes_meta.blend/mytypes.py

--- sandbox/source/py/btypes_meta.py
+++ sandbox/source/py/btypes_meta.py
@@ -3,18 +3,32 @@ import bpy
 from inspect import getfile
 
 
-def main():
-    for subclass in type.__subclasses__(type):
+class Class:
+    class Info:
+        def __init__(self, name, module, file):
+            self.name = name
+            self.module = module
+            self.file = file
+        
+        def __repr__(self):
+            kwargs = {key: getattr(self, key) for key in self.__dict__}
+            return "{name}\n\t{module}\n\t{file}\n".format(**kwargs)
+    
+    @staticmethod
+    def info(cls):
         try:
-            file = getfile(subclass)
+            file = getfile(cls)
         except TypeError as e:
             file = e
         else:
             pass
         finally:
-            name = subclass.__name__
-            module = subclass.__module__
-            print("{}\n\t{}\n\t{}\n".format(name, module, file))
+            name, module = cls.__name__, cls.__module__
+            return Class.Info(name, module, file)
+
+def main():
+    for subclass in type.__subclasses__(type):
+        print(Class.info(subclass))
 
 
 if __name__ == '__main__':
