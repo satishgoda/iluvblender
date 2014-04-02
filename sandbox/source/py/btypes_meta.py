2f7a454095bef14fcb60a8435ccd3443e4eb36fc
Satish Goda <satishgoda@gmail.com>
2014-04-02 12:27:39 +0530

    Update btypes_meta.py
    >>> import subclasses
    >>> 
    >>> subclasses.main()
    ABCMeta
        abc
        /home/satishg/bin/blenderinstalldir/blender-2.70-linux-glibc211-x86_64/2.70/python/lib/python3.3/abc.py
    
    bpy_struct_meta_idprop
        builtins
        <module 'builtins' (built-in)> is a built-in class
    
    RNAMeta
        bpy_types
        /home/satishg/bin/blenderinstalldir/blender-2.70-linux-glibc211-x86_64/2.70/scripts/modules/bpy_types.py
    
    MyMeta
        mytypes
        /home/satishg/musesatishg/blender/testing/pipeline/test1/btypes_meta.blend/mytypes.py

--- sandbox/source/py/btypes_meta.py
+++ sandbox/source/py/btypes_meta.py
@@ -3,14 +3,19 @@ import bpy
 from inspect import getfile
 
 
-for subclass in type.__subclasses__(type):
-    try:
-        file = getfile(subclass)
-    except TypeError as e:
-        file = e
-    else:
-        pass
-    finally:
-        name = subclass.__name__
-        module = subclass.__module__
-        print("{}\n\t{}\n\t{}\n".format(name, module, file))
+def main():
+    for subclass in type.__subclasses__(type):
+        try:
+            file = getfile(subclass)
+        except TypeError as e:
+            file = e
+        else:
+            pass
+        finally:
+            name = subclass.__name__
+            module = subclass.__module__
+            print("{}\n\t{}\n\t{}\n".format(name, module, file))
+
+
+if __name__ == '__main__':
+    main()
