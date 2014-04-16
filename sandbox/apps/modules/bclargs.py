2ae47c12f6254e545423b7c5870ee4e6e6da64b6
Satish Goda <satishgoda@gmail.com>
2014-04-16 16:50:24 +0530

    Update bclargs.py
    > ./bclargs.py 
    Namespace(background=False, no_native_pixels=False, render_anim=False, start_console=False, window_border=False, window_borderless=False)
    
    > ./bclargs.py --no-native-p                                                                                                                                                                                    
    Namespace(background=False, no_native_pixels=True, render_anim=False, start_console=False, window_border=False, window_borderless=False)
    
    > ./bclargs.py --help
    usage: bclargs.py [-h] [--render-anim] [--background] [--window-border]
                      [--window-borderless] [--start-console] [--no-native-pixels]
    
    Parse command-line arguments passed to Blender
    
    optional arguments:
      -h, --help            show this help message and exit
      --render-anim, -a     Render frames from start to end (inclusive)
      --background, -b      Run in background (often used for UI-less rendering)
      --window-border, -w   Force opening with borders (default)
      --window-borderless, -W
                            Force opening without borders
      --start-console, -con
                            Start with the console window open (ignored if -b is
                            set), (Windows only)
      --no-native-pixels    Do not use native pixel size, for high resolution
                            displays (MacBook 'Retina')

--- sandbox/apps/modules/bclargs.py
+++ sandbox/apps/modules/bclargs.py
@@ -8,11 +8,17 @@ parser = argparse.ArgumentParser(description="Parse command-line arguments passe
 action_store_true = (
     ('--render-anim', '-a', 'Render frames from start to end (inclusive)'),
     ('--background', '-b', 'Run in background (often used for UI-less rendering)'),
+    ('--window-border', '-w', 'Force opening with borders (default)'),
+    ('--window-borderless', '-W', 'Force opening without borders'),
+    ('--start-console', '-con', 'Start with the console window open (ignored if -b is set), (Windows only)'),
+    ('--no-native-pixels', "Do not use native pixel size, for high resolution displays (MacBook 'Retina')"),
 )
 
 
 for action in action_store_true:
-    parser.add_argument(action[0], action[1], help=action[2], action='store_true')
+    help = action[-1]
+    args = action[:-1]
+    parser.add_argument(*args, help=help, action='store_true')
 
 
 if __name__ == '__main__':
@@ -21,5 +27,4 @@ if __name__ == '__main__':
     from pprint import pprint
 
     pprint(args)
-
 
